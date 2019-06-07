fbd36055083e1306788504be186b86b732e94b9a
Adrian Popan <adrian.popan@gmail.com>
2019-06-07 05:10:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,10 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  
- 
- "Welcome to Flatiron School's Web Development Course!"
-  
+  loop do
+    number_of_times += 1 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times >= 7
+      break
+    end
+  end
 end
 
 def times_iterator(number_of_times)
